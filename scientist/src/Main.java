b6017187e6a9dd3fa351b41dff632b747d8f7f5b
simensandhaug <70633406+simensandhaug@users.noreply.github.com>
2021-12-27 00:51:18 +0100

    scientist complete

--- scientist/src/Main.java
+++ scientist/src/Main.java
@@ -1,30 +1,21 @@
 import java.util.*;
 
 public class Main {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-
-        long houses = 0;
-
-        int N = s.nextInt();
-
-        PriorityQueue<Long> priQ = new PriorityQueue<>(N);
-
-
-        long[] max3 = new long[3];
-
-        for(int i = 0; i < N; i++) priQ.add(-s.nextLong());
-
-        while(!priQ.isEmpty()) {
-            long min = Arrays.stream(max3).min().getAsLong();
-            houses += min;
-            for(int i = 0; i < 3; i++) {
-                max3[i] -= min;
-                if(max3[i] == 0 && !priQ.isEmpty()) {
-                    max3[i] = -priQ.remove();
-                }
-            }
-        }
-        System.out.println(houses);
+        int N = s.nextInt(); //Lengden av arrayet, antall typer ressurser
+        long[] r = new long[N]; //Lager ressurs arrayet
+        for(int i = 0; i < N; i++) r[i] = s.nextLong(); //Legger til alle typene ressurser til arrayet
+        Arrays.sort(r); //Sorterer så man kan finne de 2 største
+        long biggest = r[N - 1]; // Største verdien
+        long secondBiggest = r[N - 2]; //Nest største
+        long sumRest = 0; //Deklarerer summen av resten
+        for(int i = 0; i < N - 2; i++) sumRest += r[i]; //Finner summen av resten
+        //Hvis den største og nest største verdien er mindre enn summen av resten, skriver ut floor(summen av ALT / 3)
+        if(sumRest > biggest && sumRest > secondBiggest) System.out.println((int)((sumRest + biggest + secondBiggest) / 3));
+        //Hvis den største og nest største verdien er større enn summen av resten, skriver ut summen av resten
+        else if(biggest > sumRest && secondBiggest > sumRest) System.out.println(sumRest);
+        //Ellers
+        else System.out.println((int)(sumRest + secondBiggest) / 2);
     }
 }
